9fd6c2a7b73e0304e81fc270bd180492b3e32b84
tanay <tanayparihar@gmail.com>
2026-03-10 00:12:18 +0530

    minor bug fixes

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Next.js API Assignment</h1>
+
+      <p>Available APIs:</p>
+
+      <ul>
+        <li>/api/teachers</li>
+        <li>/api/students</li>
+        <li>/api/students-by-teacher/1</li>
+      </ul>
+    </div>
+  );
+}
